2e5d7265421f99257aeaf75e82172e0136346a1d
wangchuang01 <v_wangchuang01@baidu.com>
2019-07-23 10:19:17 +0800

    配置代理

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'nest-pages';
+export class AppComponent implements OnInit {
+    title = 'nest-pages';
+
+    constructor(private readonly http: HttpClient) {
+    }
+
+    ngOnInit(): void {
+        this.http.get('/cats', {
+            params: {
+                name: '王闯',
+                password: 'liang-yue',
+                number: '12345',
+                email: 'pt_liangyue@outlook.com',
+                cats: 'true',
+                numberString: '12346'
+            }
+        }).subscribe(data => {
+            console.log(data);
+        });
+    }
+
 }
